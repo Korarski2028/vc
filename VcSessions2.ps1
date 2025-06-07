4ff8366faf5f2b405fb3010176e1a365cd974c87
Korarski2028 <ekelm2004@gmail.com>
2025-06-07 18:33:32 -0300

    Delete VcSessions2.ps1

--- VcSessions2.ps1
+++
@@ -1,7 +0,0 @@
-#   Script to count sessions per Vcenter if you provide a txt file with the list
-
-#
-#
-#######################################################################################################
-# Load the list of vCenter servers
-
